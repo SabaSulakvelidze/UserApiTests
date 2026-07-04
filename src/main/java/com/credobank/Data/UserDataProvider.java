fc73055e6988958c3b5d1e2ad724995b45d7a5ff
Saba <sabasulakveidze@gmail.com>
2026-07-04 14:42:44 +0400

    feat: done

--- src/main/java/com/credobank/Data/UserDataProvider.java
+++ src/main/java/com/credobank/Data/UserDataProvider.java
@@ -10,6 +10,8 @@ public class UserDataProvider {
                 {"testFilterByAge_Positive", "age", "30", 200, 1},
                 {"testFilterByGender_Positive", "gender", "male", 200, 1},
                 {"testGetAllUsers_Positive", null, null, 200, 2},
+                // "is-Server-Down" პარამეტრი გამოიყენება სერვერის გათიშვის სიმულაციისთვის
+                // სხვა შემთხვევაში WireMock ვერ განასხვავებს ჩვეულებრივი 200-იანი სტაბისგან
                 {"testInternalServerError_Negative", "is-Server-Down", "true", 500, -1},
                 {"testInvalidAge_Negative", "age", "-1", 400, -1},
                 {"testInvalidGender_Negative", "gender", "unknown", 422, -1}
